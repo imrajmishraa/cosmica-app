b1107f6c1dd143a6ccc978c56b9c49b7ed3a6a2c
Raj Mishra <raj212558@gmail.com>
2026-07-11 22:00:50 +0530

    feat(server): add healthz route

---
+++ server/src/routes/healthz.route.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { healthz } from '../controllers/healthz.controller.js';
+
+const router = Router();
+
+router.get('/healthz', healthz);
+
+export default router;
